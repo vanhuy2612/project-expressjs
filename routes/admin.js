2e1ae4fd13785ceebd21426c7c641e3574d278a1
unknown <vanhuy2612@gmail.com>
2020-02-07 20:36:14 +0700

    Submit function Update Item

--- routes/admin.js
+++ routes/admin.js
@@ -19,7 +19,6 @@ router.get('/',function(req,res,next){
 
     var query = getCategoryQuery + getAllItemQuery;
     con.query(query, function(err,result){
-        console.log(result);
         res.render('admin',{
             title : 'Admin page',
             loggedin : req.session.loggedin,
@@ -30,6 +29,7 @@ router.get('/',function(req,res,next){
     })
     
 });
+// Get add item page
 router.get('/addItem',function(req,res){
     var con = mysql.createConnection({
         host : 'localhost',
@@ -53,6 +53,7 @@ router.get('/addItem',function(req,res){
     })
     
 });
+// Handle add item
 router.post('/addItem',function(req,res){
     var name = req.body.name;
     var price = req.body.price;
@@ -82,6 +83,61 @@ router.post('/addItem',function(req,res){
                 res.redirect('/admin');
             })
         }
+    });
+});
+// Get update item Page.
+router.get('/updateItem',function(req,res){
+    var id = req.query.id;
+
+    var con = mysql.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : '',
+        multipleStatements : true,
+        database : 'shophuyka'
+    });
+    var getItemByIDQuerry = "SELECT * FROM tblitem WHERE id = ?;"; 
+    var getCategoryQuery = 'SELECT * FROM tblcategory;';
+    var getClassifyingOfItemQuery = 'SELECT * FROM tblclassifying WhERE itemID = ?;';
+
+    var query = getItemByIDQuerry + getCategoryQuery + getClassifyingOfItemQuery;
+    con.query(query,[id,id],function(err,result){
+        res.render('updateItem',{
+            title : 'Update Item Page',
+            loggedin : req.session.loggedin,
+			username : req.session.username, 
+            item : result[0],
+            category : result[1],
+            classifying : result[2]
+        });
+    });
+});
+// Handle update item 
+router.post('/updateItem',function(req,res){
+    var id = req.body.itemID;
+    var name = req.body.name;
+    var price = req.body.price;
+    var quantity = req.body.quantity;
+    var category = req.body.category;
+    var classifying = req.body.classifying;
+    var des = req.body.des;
+    var url = req.body.urlImage;
+    console.log(id +" "+ name);
+    var con = mysql.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : '',
+        multipleStatements : true,
+        database : 'shophuyka'
+    });
+    
+    var updateItemQuery = 'UPDATE tblitem SET name=?,price=?,quantity=?,des=?,url=? WHERE id = ?;';
+    var updateClassifying = 'UPDATE tblclassifying SET name=?,categoryID=? WHERE itemID = ?;';
+    var query = updateItemQuery + updateClassifying;
+
+    con.query(query,[name,price,quantity,des,url,id,classifying,category,id],function(err,result){
+       if (result[1].length>0) console.log("Update thành công");
+       res.redirect('/admin'); 
     });
 });
 router.post('/deleteItem',function(req,res){
